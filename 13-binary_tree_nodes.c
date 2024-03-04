bf4551ae18b184eefb2bfde73ea1ffdde7e9d46e
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-03-04 17:10:58 +0100

    measures the height of a binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,11 +12,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL || tree->right != NULL)
-		nodes++;
+	nodes += (tree->left || tree->right) ? 1 : 0;
 
-	nodes = binary_tree_nodes(tree->left);
-	nodes = binary_tree_nodes(tree->right);
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
 
 	return (nodes);
 }
