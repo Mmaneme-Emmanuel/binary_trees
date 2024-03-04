cff693b5f2dac67e07b038284e0acede7f216472
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-03-04 04:12:52 +0100

    measures the height of a binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,13 +9,15 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree)
-
 	{
-		size_t depth_left = 0, depth_right = 0;
+		size_t depth = 0;
 
-		if (tree->parent)
-			return (1 + binary_tree_depth(tree->parent));
-		return (0);
+		while (tree->parent)
+		{
+			depth++;
+			tree = tree->parent;
+		}
+		return (depth);
 	}
 	return (0);
 }
